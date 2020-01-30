39f89b915627f3cbb0850343837e9114f2ba0a52
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-01-30 22:21:17 +0000

    skeleton try2

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'BumbleBees',
+        'pretty_uri' => false,
+        'site_url' => 'https://csu-php-demo.herokuapp.com',
+        'nav_menu' => [
+            'home' => 'home'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v3.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
