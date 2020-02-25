14b9e506f46a06d2b256e8a80e3689528b6e40cd
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-02-25 05:08:49 +0000

    fix5

--- template/template.php
+++ template/template.php
@@ -30,7 +30,7 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav nav-text-size">
       <?php  nav_menu(); ?>
     </ul>
