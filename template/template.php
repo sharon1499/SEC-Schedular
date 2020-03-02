4a42bfec819fd8f32caf1b1218f785dbc213c347
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-03-02 19:14:54 +0000

    addind alt to images

--- template/template.php
+++ template/template.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="/template/style.css" rel="stylesheet" type="text/css" />
     <title><?php site_name(); ?></title>
-    <link rel="shortcut icon" type="image/x-icon" href="shimane-japan-flag-symbol.svg" />
+    <link rel="shortcut icon" type="image/x-icon" href="shimane-japan-flag-symbol.svg" alt="GroupSynch Logo"/>
     </head>
   <nav class="navbar navbar-expand-lg navbar-light navbar-inverse bg-navbar-color" data-spy="affix" data-offset-top="197">
   <a class="navbar-brand" href="https://sec-schedulars.herokuapp.com//?page=home" ><img src="shimane-japan-flag-symbol.svg" height="70" width="70"/> 
