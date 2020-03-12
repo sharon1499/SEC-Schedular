66c7f1064f9dcfffe3fc3079d8738cf753b0cd69
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-03-12 02:08:22 +0000

    hightlight attempt

--- template/template.php
+++ template/template.php
@@ -12,6 +12,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery.maphilight.min.js"></script>
   
 
     <!-- Bootstrap CSS -->
