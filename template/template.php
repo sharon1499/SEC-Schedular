110e99ba67d2dfc5f61d7ebcd5a7dbaacd757ddc
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-03-12 02:27:44 +0000

    hightlight attempt

--- template/template.php
+++ template/template.php
@@ -14,6 +14,11 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/maphilight/1.4.0/jquery.maphilight.min.js"></script>
+<script>
+$(function() {
+    $('.map-area').maphilight();
+});
+</script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
