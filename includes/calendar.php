62fc9528cbc9076a847b35cd8e11ef1354c15ed6
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-03-26 17:45:56 +0000

    calendar

--- includes/calendar.php
+++ includes/calendar.php
@@ -81,12 +81,6 @@ function build_calendar($month, $year){
     echo $calendar;
 }
 ?>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-</head>
-<body>
   <div class="container">
     <div class="row">
       <div class="col-md-12">
@@ -99,5 +93,3 @@ function build_calendar($month, $year){
       </div>
     </div>
   </div>
-</body>
-</html>
